4d76e1518a2c8efce3ece67a544885333bad9bf3
Brian Suh <brian.j.suh@gmail.com>
2013-11-16 23:12:01 -0800

    fix runners

--- run-headless.py
+++ run-headless.py
@@ -12,15 +12,16 @@ if __name__ == '__main__':
         print 'usage: python run.py <usercode1.py> <usercode2.py> [<map file>]'
         sys.exit()
 
-    players = [make_player(x) for x in sys.argv[1:3]]
-    g = game.Game(*players, record_turns=False)
-
     map_name = os.path.join(os.path.dirname(__file__), 'maps/default.py')
     if len(sys.argv) > 3:
         map_name = sys.argv[3]
 
     map_data = ast.literal_eval(open(map_name).read())
     game.init_settings(map_data)
+
+    players = [make_player(x) for x in sys.argv[1:3]]
+    g = game.Game(*players, record_turns=False)
+
     for i in range(settings.max_turns):
         g.run_turn()
 
