7994a5d39fd7a55b3f353060d2e6b16b142f65be
Brian Suh <brian.j.suh@gmail.com>
2013-11-18 00:10:48 -0800

    improve performance

--- rg.py
+++ rg.py
@@ -23,6 +23,15 @@ def set_settings(s):
 dist = lambda p1, p2: math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
 wdist = lambda p1, p2: abs(p2[0]-p1[0]) + abs(p2[1]-p1[1])
 
+def memodict(f):
+    """ Memoization decorator for a function taking a single argument """
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+    return memodict().__getitem__
+
+@memodict
 def loc_types(loc):
     for i in range(2):
         if not (0 <= loc[i] < settings.board_size):
@@ -34,16 +43,15 @@ def loc_types(loc):
         types.append('obstacle')
     return types
 
+@memodict
+def _locs_around(loc):
+    offsets = ((0, 1), (1, 0), (0, -1), (-1, 0))
+    return [tuple(map(operator.add, loc, o)) for o in offsets]
+
 def locs_around(loc, filter_out=None):
     filter_out = filter_out or []
-    offsets = ((0, 1), (1, 0), (0, -1), (-1, 0))
-    locs = []
-
-    for o in offsets:
-        new_loc = tuple(map(operator.add, loc, o))
-        if len(set(filter_out) & set(loc_types(new_loc))) == 0:
-            locs.append(new_loc)
-    return locs
+    return [loc for loc in _locs_around(loc) if
+            len(set(filter_out) & set(loc_types(loc))) == 0]
 
 def toward(curr, dest):
     if curr == dest:
