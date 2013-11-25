02c827d7a4b94ecccddf83ff86559e40e909023e
System Administrator <root@macbook.local>
2013-11-24 19:10:45 -0800

    removed different types of robots (e.g. TankRobot) from the game

--- defaultrobots.py
+++ defaultrobots.py
@@ -1,13 +1,3 @@
-from settings import settings
-
-class Commander:
-    def spawn(self, game):
-        return ['Robot'] * settings.spawn_per_player
-
 class Robot:
-    def act(self, game):
-        return ['guard']
-
-class TankRobot:
     def act(self, game):
         return ['guard']
